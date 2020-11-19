ffff9439c4f97adfae09b50070b9f7554d02b0a5
kimmymonassar <kimmy.monassar@soleilit.se>
2020-11-19 14:34:47 +0100

    test(local): add local only run tests

--- .jest/testEnvLoader.ts
+++ .jest/testEnvLoader.ts
@@ -1,4 +1,4 @@
 import { config } from 'dotenv';
 import path from 'path';
 
-config({ path: path.resolve(process.cwd(), '.env') });
+config({ path: path.resolve(process.cwd(), '.env.test') });
--- __tests__/api/caseOpening/calculateCaseItem.spec.ts
+++ __tests__/api/caseOpening/calculateCaseItem.spec.ts
@@ -1,6 +1,7 @@
 import { connect, connection } from 'mongoose';
 import getCsgoCase from '../../../src/db/getCaseFromDb';
 const uri = `${process.env.MONGODB_URL}:${process.env.MONGO_PORT}${process.env.MONGO_DB_PATH}`;
+const isRunningLocally = false;
 
 async function initalizeDb() {
   await connect(`${uri}`, {
@@ -8,6 +9,7 @@ async function initalizeDb() {
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
+    poolSize: 10,
   });
   console.log('MongoDB connection is up');
 }
@@ -28,11 +30,35 @@ describe('calculateCaseItem.ts', () => {
   test('it should get one item from the csgo case', async () => {
     try {
       const item = await getCsgoCase('Fracture Case');
-      console.log(JSON.stringify(item));
 
       expect(item).toBeTruthy();
     } catch (e) {
       console.error(e);
     }
   });
+
+  // Only run this test locally
+  if (isRunningLocally) {
+    test('it should get one knife in 10000 tries', async () => {
+      try {
+        let knife = null;
+
+        for (let i = 0; i < 10000; i++) {
+          const item: any = await getCsgoCase('Fracture Case');
+
+          if (item && item.name.includes('Knife')) {
+            knife = item;
+            console.log(item);
+          }
+        }
+
+        expect(knife).toBeTruthy();
+      } catch (e) {
+        console.error(e);
+      }
+    }, 500000);
+  }
 });
+
+// https://mongoosejs.com/docs/connections.html
+//buffercommands
